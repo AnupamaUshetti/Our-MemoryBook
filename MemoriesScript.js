e55443b44279f4a9a9e6d401b3236ff3c99a8e47
Your Name <anupamaush69@gmail.com>
2025-02-07 16:37:09 +0530

    Added javascript file to style Home page.

--- MemoriesScript.js
+++ MemoriesScript.js
@@ -1,7 +1,39 @@
 // Array of photos with their descriptions
 const galleryPhotos = [
   {
-      imgSrc: '/images/memories/birthday_cards.jpg',
+      imgSrc: '/images/memories/first_photo.jpg',
+      description: 'Beautiful sunset at the beach'
+  },
+  {
+      imgSrc: '/images/memories/our_first_call.jpeg',
+      description: 'Mountain landscape in autumn'
+  },
+  {
+      imgSrc: '/images/memories/viva_kiss.jpg',
+      description: 'City lights at night'
+  },
+  {
+      imgSrc: '/images/memories/video_calls.jpg',
+      description: 'bla'
+  },
+  {
+    imgSrc: '/images/memories/Sure my queen.jpg',
+    description: 'Beautiful sunset at the beach'
+  },
+  {
+      imgSrc: '/images/memories/first_gift_forMe.jpg',
+      description: 'Mountain landscape in autumn'
+  },
+  {
+      imgSrc: '/images/memories/different_icecream.jpg',
+      description: 'City lights at night'
+  },
+  {
+      imgSrc: '/images/memories/my_little_pookie.jpeg',
+      description: 'bla'
+  },
+  {
+      imgSrc: '/images/memories/happy_birthday_sweetheart.jpg',
       description: 'Beautiful sunset at the beach'
   },
   {
@@ -9,11 +41,59 @@ const galleryPhotos = [
       description: 'Mountain landscape in autumn'
   },
   {
-      imgSrc: '/images/memories/gift_that_make_you_happy.jpg',
+      imgSrc: '/images/memories/birthday_cards.jpg',
       description: 'City lights at night'
   },
   {
-      imgSrc: '/images/memories/my_little_pookie.jpeg',
+      imgSrc: '/images/memories/pinky_promises.jpg',
+      description: 'bla'
+  },
+  {
+      imgSrc: '/images/memories/gift_that_make_you_happy.jpg',
+      description: 'Beautiful sunset at the beach'
+  },
+  {
+      imgSrc: '/images/memories/rabbit_head.jpg',
+      description: 'Mountain landscape in autumn'
+  },
+  {
+      imgSrc: '/images/memories/snap_to_remember.jpg',
+      description: 'City lights at night'
+  },
+  {
+      imgSrc: '/images/memories/little_things_makeMeHappy.jpg',
+      description: 'bla'
+  },
+  {
+      imgSrc: '/images/memories/not_just_wallpaper.jpg',
+      description: 'Beautiful sunset at the beach'
+  },
+  {
+      imgSrc: '/images/memories/flower_girl.jpg',
+      description: 'Mountain landscape in autumn'
+  },
+  {
+      imgSrc: '/images/memories/great_satisfaction.jpeg',
+      description: 'City lights at night'
+  },
+  {
+      imgSrc: '/images/memories/pinky_promises.jpg',
+      description: 'bla'
+  },
+  {
+      imgSrc: '/images/memories/gift_that_make_you_happy.jpg',
+      description: 'Beautiful sunset at the beach'
+  },
+  {
+      imgSrc: '/images/memories/rabbit_head.jpg',
+      description: 'Mountain landscape in autumn'
+  },
+  {
+      imgSrc: '/images/memories/snap_to_remember.jpg',
+      description: 'City lights at night'
+  },
+  {
+      imgSrc: '/images/memories/little_things_makeMeHappy.jpg',
       description: 'bla'
   }
 ];
