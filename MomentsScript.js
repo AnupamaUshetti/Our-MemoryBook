87f6de4c97252608582617d1b67bde75ea4dd5cc
Your Name <anupamaush69@gmail.com>
2025-02-11 04:44:50 +0530

    Finshed adding descriptions to the Moments page.

--- MomentsScript.js
+++ MomentsScript.js
@@ -2,43 +2,43 @@
 const galleryPhotos = [
     {
         imgSrc: '/images/moments/galleface.jpg',
-        description: 'Beautiful sunset at the beach'
+        description: 'The very first day we went to Galle Face.🌊❤️'
     },
     {
         imgSrc: '/images/moments/first_royal_event.jpg',
-        description: 'Mountain landscape in autumn'
+        description: 'Our first Royal event as a couple!💙💛💙 You looked a bit less interested that day. But baba, there are so many more events to enjoy.😉✨'
     },
     {
         imgSrc: '/images/moments/selfie_time.jpg',
-        description: 'City lights at night'
+        description: 'The day after a moment I truly smiled with you.😊❤️'
     },
     {
         imgSrc: '/images/moments/chill_duo.jpg',
-        description: 'bla'
+        description: 'Just us, chilling together.❄️✨'
     },
     {
       imgSrc: '/images/moments/rio.jpeg',
-      description: 'Beautiful sunset at the beach'
+      description: 'The day we went to Rio. Good vibes, great food, and memories worth keeping.🍧✨'
     },
     {
         imgSrc: '/images/moments/gal_beach.jpg',
-        description: 'Mountain landscape in autumn'
+        description: 'What was that place? But I still got that feeling on that day.✨'
     },
     {
         imgSrc: '/images/moments/bandi_full.jpeg',
-        description: 'City lights at night'
+        description: 'Al-Mass biriyani + 2 full Bandi gedies.🍽️😋'
     },
     {
         imgSrc: '/images/moments/sleeping _baby.jpeg',
-        description: 'bla'
+        description: 'Awwww... Who is that sleeping cutieee?💤😴💖'
     },
     {
         imgSrc: '/images/moments/LostInOurWorld.jpg',
-        description: 'City lights at night'
+        description: "Yes, it's Port City. But on that day, it was our own world. How can I forget those feelings and memories? 🏙️💖"
     },
     {
-        imgSrc: '/images/moments/MaaruDeepnSheiko.jpg',
-        description: 'bla'
+        imgSrc: '/images/moments/MaaruDeepnShiok.jpg',
+        description: 'A flavorful meal in our own little world.🍽️💫 Maaru deepn Shiok.😅'
     }
   ];
   
